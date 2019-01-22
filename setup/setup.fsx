034a21ee6c889e2add712824c66ce9b18616cf43
Don Syme <dsyme@microsoft.com>
2019-01-22 17:19:24 +0000

    rejig projects a little

--- setup/setup.fsx
+++ setup/setup.fsx
@@ -5,7 +5,6 @@ open NugetDownload
 
 let nugetFiles = 
     [| 
-        "ionic.zlib/1.9.1.5", [|"lib/Ionic.Zlib.dll"|]
         "HDF.PInvoke.NETStandard/1.10.200",
         [|
             yield @"lib/netstandard2.0/HDF.PInvoke.dll"
